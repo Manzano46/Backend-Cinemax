efbab507a0df8bd5c08785ad88265dbad3858e0b
Manzano46 <kevinmanzano020610@gmail.com>
2024-04-23 15:16:29 -0400

    feat: asd

--- Cinemax.Api/Common/Mapping/PaymentTypeMappingConfig.cs
+++ Cinemax.Api/Common/Mapping/PaymentTypeMappingConfig.cs
@@ -1,6 +1,7 @@
 using Cinemax.Application.PaymentTypes.Commands.Create;
 using Cinemax.Application.PaymentTypes.Common;
 using Cinemax.Contracts.PaymentTypes;
+using Cinemax.Domain.PaymentType.Entities;
 using Mapster;
 
 namespace Cinemax.Api.Common.Mapping;
@@ -14,5 +15,9 @@ public class PaymentTypeMappingConfig : IRegister
         config.NewConfig<CreatePaymentTypeRequest, CreatePaymentTypeCommand>()
         .Map(dest => dest.Name, src => src.Name);
 
+        config.NewConfig<PaymentType, PaymentTypeResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value )
+            .Map(dest => dest.Name, src => src.Name);
+           
     }
 }
